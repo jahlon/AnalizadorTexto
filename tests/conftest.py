0b5f0a1bc7d694e7d30a35815c7929bfb76efa98
jahlonteaching <jehincapie@udemedellin.edu.co>
2023-05-29 16:05:45 -0500

    Add logic to generate reporte for each tested file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,10 @@
+import os
+from importlib.resources import files
+from pathlib import Path
+
+import pytest
+
+
 def pytest_addoption(parser):
     parser.addoption("--module-file", action="store", default="default name")
     parser.addoption("--name", action="store", default="default name")
@@ -5,3 +12,56 @@ def pytest_addoption(parser):
 
 def pytest_sessionstart(session):
     session.results = dict()
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    result = outcome.get_result()
+
+    if result.when == "call" or result.when == "setup":
+        item.session.results[item] = result
+
+
+def pytest_sessionfinish(session, exitstatus):
+    results_by_test = dict()
+    for result in session.results.values():
+        test_name: str = result.nodeid.split("::")[1]
+        index = test_name.find("[")
+        if index > -1:
+            key = test_name[:index]
+        else:
+            key = test_name
+
+        if key in results_by_test.keys():
+            results_by_test[key]["total"] += 1
+            if result.outcome == "passed":
+                results_by_test[key]["passed"] += 1
+        else:
+            results_by_test[key] = {"total": 1, "passed": 1 if result.outcome == "passed" else 0}
+
+    report_file_name = session.config.getoption("--name")[:-3]
+    path = Path(files("tests").joinpath(Path("resultados")))
+    if not path.exists():
+        os.mkdir(path)
+
+    with open(f"{path}{os.sep}{report_file_name}.txt", mode="w", encoding="utf8") as file:
+        total_tests = len(results_by_test)
+        score_sum = 0
+        line_char = u'\u2500' * 93
+        c_char = u'\u2502'
+        header = f"{line_char}\n{c_char}{'#':^3}{c_char}{' Prueba':<78}{c_char}{'Puntos':^8}{c_char}\n{line_char}\n"
+        file.write(header)
+        for count, item in enumerate(results_by_test.items(), start=1):
+            key, value = item
+            test_score = value["passed"] / value["total"]
+            score_sum += test_score
+            inter_test = f"({value['passed']}/{value['total']})"
+            line = f"{c_char}{count:<3}{c_char} {key:<70}{inter_test:<7}{c_char}{test_score:^8.1f}{c_char}\n"
+            file.write(line)
+
+        file.write(f"{line_char}\n")
+        footer = f"{c_char}{'TOTAL':<82}{c_char}{score_sum:^8.1f}{c_char}\n{line_char}\n"
+        file.write(footer)
+        score = score_sum / total_tests * 5
+        file.write(f"NOTA: {score_sum:.1f} / {total_tests} = {score: .1f}")
--- tests/invoker.py
+++ tests/invoker.py
@@ -1,13 +1,17 @@
 import os
 from importlib.resources import files
+from pathlib import Path
 
 import pytest
 
 
 def invoke_tests():
-    directory = str(files('analizador.modelo'))
-    file = "analizador.py"
-    pytest.main(["test_analizador.py", "--module-file", f"{directory}{os.sep}{file}", "--name", file])
+    directory = str(files('analizador').joinpath(Path("entregas")))
+    file_list = os.listdir(directory)
+
+    for file in file_list:
+        if not file.startswith("__"):
+            pytest.main(["test_analizador.py", "--module-file", f"{directory}{os.sep}{file}", "--name", file])
 
 
 if __name__ == "__main__":
